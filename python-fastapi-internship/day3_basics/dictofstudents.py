cc35ab7bec943bd91925ba834ac6a0007b2a83a4
Muhammed Rahees N <raheesnelliyali01@gmail.com>
2026-05-29 15:43:48 +0530

    Delete python-fastapi-internship/day3_basics directory

--- python-fastapi-internship/day3_basics/dictofstudents.py
+++
@@ -1,4 +0,0 @@
-students={"sahad":30,"rahees":28,"refai":25,"saad":27}
-for name, marks in students.items():
-    if marks >= 28:
-        print(name, "passed")
--- python-fastapi-internship/day3_basics/wordfrequency.py
+++
@@ -1,6 +0,0 @@
-sentence=("Sahad is a handsome boy")
-word=sentence.split()
-count={}
-for i in word:
-    count[i] = count.get(i, 0) + 1
-print(count)
